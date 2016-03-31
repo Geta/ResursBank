3ab8a2c44ac2fa08b671247ac78e305005e4d3f7
thien-trinh-geta <thien.trinh@geta.no>
2016-03-31 11:35:32 +0700

    Add properties to ResursBankPayment

--- src/Geta.Epi.Commerce.Payments.Resurs.Checkout/Bussiness/ResursBankPayment.cs
+++ src/Geta.Epi.Commerce.Payments.Resurs.Checkout/Bussiness/ResursBankPayment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Geta.Resurs.Checkout.Model;
 using Mediachase.Commerce.Orders;
 
 namespace Geta.Epi.Commerce.Payments.Resurs.Checkout.Bussiness
@@ -10,5 +11,16 @@ namespace Geta.Epi.Commerce.Payments.Resurs.Checkout.Bussiness
     [Serializable]
     public class ResursBankPayment: OtherPayment
     {
+        public string PreferredId { get; set; }
+        public string CustomerIpAddress { get; set; }
+        public bool WaitForFraudControl { get; set; }
+        public bool AnnulIfFrozen { get; set; }
+        public bool FinalizeIfBooked { get; set; }
+        public List<SpecLine> SpecLines { get; set; }
+        public Customer Customer { get; set; }
+        public string SuccessUrl { get; set; }
+        public string FailUrl { get; set; }
+        public string ForceSigning { get; set; }
+        public string CallBackUrl { get; set; }
     }
 }
