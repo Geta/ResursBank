dd37b93414295ce773f7b60f3c8758daa317b335
thien-trinh-geta <thien.trinh@geta.no>
2016-04-04 18:19:54 +0700

    add JS, CSS for partial view

---
+++ demo/Quicksilver/Sources/EPiServer.Reference.Commerce.Site/Scripts/js/Payment.js
@@ -0,0 +1,26 @@
+﻿var Payment = {
+    init: function () {
+        var allOptions = $('.payment-method__option-item .radio__container .radio__input');
+        if (allOptions && allOptions.length > 0) {
+            for (var i = 0; i < allOptions.length; i++) {
+                (function (option, index, allOptions) {
+                    option.on('click', function () {
+                        for (var i = 0; i < allOptions.length; i++) {
+                            if (i != index) {
+                                var detail = allOptions.eq(i).parent().parent().children('.payment-method__detail-container');
+                                if (detail && detail.length > 0 && detail.css('display') != 'none') {
+                                    detail.slideUp(250);
+                                }
+                            }
+                        }
+
+                        var detail = $(this).parent().parent().children('.payment-method__detail-container');
+                        if (detail && detail.length > 0 && detail.css('display') == 'none') {
+                            detail.slideDown(250);
+                        }
+                    });
+                })(allOptions.eq(i), i, allOptions);
+            }
+        }
+    }
+};
