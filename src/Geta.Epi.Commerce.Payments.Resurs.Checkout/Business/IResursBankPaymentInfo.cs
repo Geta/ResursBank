b212342cb53ad96ccdd2786a490d2c5663ec7677
Brian Weeteling <brianweeteling@gmail.com>
2018-06-14 15:25:27 +0200

    Add payment info

---
+++ src/Geta.Epi.Commerce.Payments.Resurs.Checkout/Business/IResursBankPaymentInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Geta.Epi.Commerce.Payments.Resurs.Checkout.Business
+{
+    public interface IResursBankPaymentInfo
+    {
+        decimal? AmountForNewCard { get; set; }
+        string GovernmentId { get; set; }
+        string InvoiceDeliveryType { get; set; }
+        string ResursPaymentMethod { get; set; }
+        string SigningSuccessUrl { get; set; }
+        string SigningFailedUrl { get; set; }
+    }
+}
